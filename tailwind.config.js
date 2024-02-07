ea666571966891bf44fc7b94d5987570beaf5a26
byt3h3ad <adhirajd007@gmail.com>
2024-02-07 12:05:58 +0530

    fix: change font to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './src/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        Inter: ['Inter', 'sans-serif']
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         "vermilion": {
